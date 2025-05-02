6319d9d5b2cc9c6019a49e5a70c1e5d0d17a82c3
natyyHy <natielegrazielly5@gmail.com>
2025-05-02 18:07:29 -0300

    refactor: continuando o codico crawler (esta na metade)

--- crawler.js
+++ crawler.js
@@ -9,11 +9,17 @@ const dadosPaginas = [];
 async function crawlerPagina(urlPagina) { // definindo uma funcao assincrona
     try {
         const resposta = await axios.get(urlPagina); // axios.get faz a requisicao http e buscar o conteudo da pagina; await esperar a resposta chegar
-        console.log(resposta.data) 
-        const $ = cheerio.load(resposta.data);
+        const $ = cheerio.load(resposta.data); // transforma o html em um objeto que pode ser manipulado; a variavel $ permite buscar os elementos do html,como $('a') e $('p')
         
+        const links = [];
+        $('a').each( (i, elemento) => { // $('a') pega todos os elementos <a>, e each itera sobre cada <a>
+            const texto = $(elemento).text();
+        })
 
     }catch (erro) {
         console.error('erro ao acessar pagina: ',erro.messege);
     }
 }
+
+
+crawlerPagina('https://github.com/natyyHy/crawler-buscador-filmes-ficcao/blob/c7c2e269f16835effbfd35a9452dcce3dcf8130d/pages/blade_runner.html')
